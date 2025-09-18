040e75adbaadfc4dc19da1b2a2c1688d7ec72c02
Shreyas Kotian <shreyaskotian09@gmail.com>
2025-09-18 07:55:05 +0530

    Structs

--- src/bin/struct_rect.rs
+++ src/bin/struct_rect.rs
@@ -7,6 +7,10 @@ impl Rect {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn perimeter(&self) -> u32 {
+        2 * (self.width + self.height)
+    }
 }
 
 fn main() {
@@ -17,5 +21,10 @@ fn main() {
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
+    );
+
+    println!(
+        "The perimeter of the rectangle is {} pixels.",
+        rect1.perimeter()
     );
 }
